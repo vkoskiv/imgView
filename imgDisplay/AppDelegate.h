1c69afcc87bed15dd64692af36a3a45e611271bf
Valtteri Koskivuori <vkoskiv@gmail.com>
2016-06-24 01:43:29 +0300

    - Initial commit

---
+++ imgDisplay/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  imgDisplay
+//
+//  Created by Valtteri Koskivuori on 23/06/16.
+//  Copyright © 2016 Valtteri Koskivuori. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ imgDisplay/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  imgDisplay
+//
+//  Created by Valtteri Koskivuori on 23/06/16.
+//  Copyright © 2016 Valtteri Koskivuori. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
